294d60f5babd39008c261c12341fdbc47040daf9
BrandonLozada <brandon_lozada@outlook.com>
2024-04-02 22:49:24 -0600

    Crear EndPoints de createService y getServiceById

--- controllers/servicesController.js
+++ controllers/servicesController.js
@@ -1,10 +1,61 @@
+import mongoose from 'mongoose'
 import { services } from '../data/beautyServices.js'
+import Services from '../models/Services.js'
 
+const createService = async (req, res) => {
+    if(Object.values(req.body).includes('')) {
+        const error = new Error('Todos los campos son obligatorios.')
+
+        return res.status(400).json({
+            message: error.message
+        })
+    }
+
+    try {
+        const service = new Services(req.body)
+        await service.save()
+
+        res.json({
+            message: 'El servicio se creó correctamente.'
+        })
+    } catch (error) {
+        return res.status(400).json({
+            message: error
+        })
+    }
+}
 
 const getServices = (req, res) => {
     res.json(services)
 }
 
+const getServiceById = async (req, res) => {
+    const { id } = req.params
+    // Validar un Object id.
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        const error = new Error('El identificador no es válido.')
+
+        return res.status(400).json({
+            message: error.message
+        })
+    }
+
+    // Validar que existe.
+    const service = await Services.findById(id)
+    if(!service) {
+        const error = new Error('El servicio no existe.')
+
+        return res.status(404).json({
+            message: error.message
+        })
+    }
+
+    // Mostra el servicio.
+    res.json(service)
+}
+
 export {
-    getServices
+    getServices,
+    createService,
+    getServiceById
 }
--- index.js
+++ index.js
@@ -4,22 +4,25 @@ import colors from 'colors'
 import { db } from './config/db.js'
 import servicesRoutes from './routes/servicesRoutes.js'
 
-// Environment variables
+// Environment variables.
 dotenv.config()
 
 // Configure the app.
 const app = express()
 
-// Connect to DB
+// read received data through body request.
+app.use(express.json())
+
+// Connect to DB.
 db()
 
 // Define a route.
 app.use('/api/services', servicesRoutes)
 
-// Define port
+// Define port.
 const PORT = process.env.PORT || 4000
 
-// Run the app
+// Run the app.
 app.listen(PORT, () => {
     console.log(colors.blue('El servidor se está ejecutando en el puerto:', colors.blue.bold(PORT)))
 })
