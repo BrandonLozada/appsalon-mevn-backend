eceb6b84eaa68e7c4d4be187c2a49fbee73e5d06
BrandonLozada <brandon_lozada@outlook.com>
2024-04-07 23:24:35 -0600

    Instalar y configurar CORS para permitir peticiones desde otros origenes como localhost y postman en modo desarrollo

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import cors from 'cors'
 import { db } from './config/db.js'
 import servicesRoutes from './routes/servicesRoutes.js'
 
@@ -15,6 +16,24 @@ app.use(express.json())
 
 // Connect to DB.
 db()
+
+// Configure CORS
+// TODO: Remove the "undefined" item from whiteList array.
+const whiteList = process.argv[2] === '--postman' ? [process.env.FRONTEND_URL, undefined] : [process.env.FRONTEND_URL]
+
+const corsOptions = {
+    origin: function(origin, callback) {
+        if(whiteList.includes(origin)) {
+            // Allow the request
+            callback(null, true)
+        } else {
+            // Reject the request
+            callback(new Error('Error de CORS'))
+        }
+    }
+}
+
+app.use(cors(corsOptions))
 
 // Define a route.
 app.use('/api/services', servicesRoutes)
